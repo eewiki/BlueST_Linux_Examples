2aee2e78ec43e7740b703cd37e8d10814b17e32f
Robert Nelson <robertcnelson@gmail.com>
2020-04-27 17:23:16 -0500

    try speeding up reconnect timeout
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- simple_logger/logger.sh
+++ simple_logger/logger.sh
@@ -9,11 +9,11 @@ while :
 do
 	date_stamp=$(date +%Y.%m.%d.%H.%M.%S)
 	python3 grab_accel.py >> /var/www/html/accel-${date_stamp}.log
+	sync
 	if [ -f /tmp/stop ] ; then
 		exit
 	fi
 	cat /var/www/html/accel-${date_stamp}.log | grep "^#Device BAI_400 disconnected unexpectedly." && sleep 5
 	cat /var/www/html/accel-${date_stamp}.log | grep "^#No Bluetooth devices found. Exiting..." && exit
-	sleep 1
 done
 
