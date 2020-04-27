4bee208c0490ac5c167ae3ac4e9b095b868c003d
Robert Nelson <robertcnelson@gmail.com>
2020-04-27 17:15:09 -0500

    lets look for a file to stop
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- simple_logger/logger.sh
+++ simple_logger/logger.sh
@@ -9,7 +9,10 @@ while :
 do
 	date_stamp=$(date +%Y.%m.%d.%H.%M.%S)
 	python3 grab_accel.py >> /var/www/html/accel-${date_stamp}.log
+	if [ -f /tmp/stop ] ; then
+		exit
+	fi
 	cat /var/www/html/accel-${date_stamp}.log | grep "^#Device BAI_400 disconnected unexpectedly." && exit
-	sleep 1
+	cat /var/www/html/accel-${date_stamp}.log | grep "^#No Bluetooth devices found. Exiting..." && exit
 done
 
